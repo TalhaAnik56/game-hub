650f24c91f2a3612152495c50d1f9709f45218e7
TalhaAnik56 <aniktalha@gmail.com>
2023-08-24 03:32:08 +0600

    Create a generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,14 +2,14 @@ import { Spinner } from "@chakra-ui/react"
 import useGenres from "../hooks/useGenres"
 
 const GenreList = () => {
-    const {isLoading,genres,error}=useGenres()
+    const {data,isLoading,error}=useGenres()
   return (
     <>
 
     {isLoading && <Spinner></Spinner>}
     {error && <h2>{error}</h2>}
     <ul>
-        {genres.map(genre=> <li key={genre.id}>{genre.name}</li>)}
+        {data.map(genre=> <li key={genre.id}>{genre.name}</li>)}
     </ul>
     </>
   )
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import apiClient, { CanceledError } from "../services.ts/api-client";
+import useData from "./useData";
 
 export interface Platform{
   id:number;
   name:string;
   slug:string
 }
-
 
 
 export interface Game {
@@ -17,36 +17,6 @@ export interface Game {
   metacritic:number
 }
 
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading,setLoading]=useState(false)
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true)
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) =>{
-        setGames(res.data.results)
-        setLoading(false)
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false)
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error,isLoading };
-};
+const useGames=()=>useData<Game>('/games')
 
 export default useGames;
