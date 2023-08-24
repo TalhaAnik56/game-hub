80cc1581f0984e549eb1f4efa6b192f27d0c1123
TalhaAnik56 <aniktalha@gmail.com>
2023-08-25 04:37:21 +0600

    Changed the logo of home page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
-import { Image,HStack } from "@chakra-ui/react"
-import logo from '../assets/logo2.webp'
+import { Image,HStack, Text } from "@chakra-ui/react"
+import logo from '../assets/mogo.webp'
 import ColorModeSwitch from "./ColorModeSwitch"
 
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="20px">
-        <Image src={logo} boxSize='60px'/>
+        <Image src={logo} boxSize='80px'/>
         <ColorModeSwitch/>
     </HStack>
   )
