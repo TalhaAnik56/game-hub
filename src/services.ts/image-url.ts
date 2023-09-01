f2fd3a78950bfc4fe946506802327f5c69e98df0
TalhaAnik56 <aniktalha@gmail.com>
2023-09-01 13:09:22 +0600

    Handling games without an image

--- src/services.ts/image-url.ts
+++ src/services.ts/image-url.ts
@@ -1,4 +1,8 @@
-const getCroppedImageUrl = (url: string) => {
+import noImage from '../assets/noImagePlaceholder.jpg'
+
+const getCroppedImageUrl = (url: string|null) => {
+  if (url===null) return noImage
+
   let index = url.indexOf("media/") + "media/".length;
   const newUrl = url.slice(0, index) + "crop/600/400/" + url.slice(index);
   return newUrl;
