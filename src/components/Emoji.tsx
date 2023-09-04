7e6e891973c8f0165bf5e6a332a6e8a5daccae8f
TalhaAnik56 <aniktalha@gmail.com>
2023-09-04 16:05:34 +0600

    done with all the things

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,4 +1,4 @@
-import { Image, ImgProps } from "@chakra-ui/react";
+import { Image, ImageProps} from "@chakra-ui/react";
 import bullsEye from "../assets/bulls-eye.webp";
 import meh from "../assets/meh.webp";
 import thumbsUp from "../assets/thumbs-up.webp";
@@ -8,9 +8,9 @@ interface Props {
 }
 
 const Emoji = ({ rating }: Props) => {
-  if (rating < 2) return;
+  if (rating < 3) return null
 
-  const emojiMap: { [key: number]: ImgProps } = {
+  const emojiMap: { [key: number]: ImageProps } = {
     3: { src: meh, alt: "meh", boxSize: "32px" },
     4: { src: thumbsUp, alt: "thumbsUp", boxSize: "32px" },
     5: { src: bullsEye, alt: "bullsEye", boxSize: "42px" },
